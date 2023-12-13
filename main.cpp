a2acbb5f1933d8eb99f2f4b920af97b241e07564
stesleva <stesleva123456789@gmail.com>
2023-12-13 10:57:07 +0200

    structure defined

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "snake.cpp"
+#include "map.cpp"
+#include "snake.h"
+#include "map.h"
+#include "apple.h"
+#include "engine.h"
+#include <iostream>
+
+
+int main()
+{
+	engine _engine;
+	_engine.Play();
+
+
+	return 0;
+}
